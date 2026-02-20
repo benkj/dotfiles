921467a7dde8c5f997837371db5a080a83c1b8cf
Leonardo Banchi <benkj.it@gmail.com>
2026-02-20 12:55:31 +0100

    ls update + tab move

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -4,6 +4,8 @@ vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>', { desc = 'P
 vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>', { desc = 'Next diagnostics' })
 
 
--- Move around buffers
-vim.keymap.set('n', '[b', '<cmd>:bprev<cr>', { desc = 'Prev buffer' })
-vim.keymap.set('n', ']b', '<cmd>:bnext<cr>', { desc = 'Next buffer' })
+-- Move around buffers and tabs
+vim.keymap.set('n', '[b', '<cmd>:bprev<cr>',   { desc = 'Prev buffer' })
+vim.keymap.set('n', ']b', '<cmd>:bnext<cr>',   { desc = 'Next buffer' })
+vim.keymap.set('n', '[t', '<cmd>:tabprev<cr>', { desc = 'Prev tab' })
+vim.keymap.set('n', ']t', '<cmd>:tabnext<cr>', { desc = 'Next tab' })
